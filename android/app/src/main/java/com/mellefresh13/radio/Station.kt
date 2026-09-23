5658d5fe22b8b1199fab6f07ba1b32bec8dc4029
mellefresh13-tech <mellefresh13@gmail.com>
2026-09-23 14:17:44 +0300

    feat: add Android station domain models and demo catalog

---
+++ android/app/src/main/java/com/mellefresh13/radio/Station.kt
@@ -0,0 +1,16 @@
+package com.mellefresh13.radio
+
+data class Station(
+    val id: String,
+    val name: String,
+    val country: String,
+    val countryCode: String,
+    val city: String,
+    val genre: String,
+    val language: String,
+    val streams: List<String>,
+    val songTitle: String? = null,
+    val artist: String? = null,
+    val website: String? = null,
+    var favorite: Boolean = false
+)
